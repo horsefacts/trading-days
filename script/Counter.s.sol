b5ba8bf44e7425541090b1bdbee4f5174d758d8a
horsefacts <109845214+horsefacts@users.noreply.github.com>
2023-07-01 19:54:41 -0400

    Initial commit

---
+++ script/Counter.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "forge-std/Script.sol";
+
+contract CounterScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+}
